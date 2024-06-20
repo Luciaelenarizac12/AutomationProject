02ab3bb522ff9299fdec87a0be855c740eaaa714
Mihai Luca <luciarelena@yahoo.com>
2024-06-20 20:46:59 +0300

    Am terminat testul legat de IFrame

---
+++ src/test/java/HelperMethods/FrameMethods.java
@@ -0,0 +1,25 @@
+package HelperMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FrameMethods {
+
+    public WebDriver driver;
+
+    public FrameMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchtoFrame(String text){
+        driver.switchTo().frame(text);
+    }
+
+    public void switchtoFrame(WebElement element){
+        driver.switchTo().frame(element);
+    }
+
+    public void defaultcontentMethod (){
+        driver.switchTo().defaultContent();
+    }
+}
